ef6f3a002dfc7396e85aba601ce029ace8b05d59
Alexander Kell <alexander@kell.es>
2020-05-30 11:02:12 +0100

    pyenv(ExecutionMode,OutOfProcess)

--- FTT61x24v8.1FTC/Run_FTT_Power.m
+++ FTT61x24v8.1FTC/Run_FTT_Power.m
@@ -6,6 +6,8 @@ function observations = Run_FTT_Power(action, input_NWR, input_NET)
 % while true
 % for runner = 1:500
 % %     Initialise reinforcment learning client
+    pyenv("ExecutionMode","OutOfProcess")
+    
     import ray.rllib.env.policy_client.*
     import gym.*
     import numpy.*
