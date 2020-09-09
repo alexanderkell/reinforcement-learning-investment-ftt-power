7be5e5d3a00fd10fd14e9760de28273af1c62991
Alexander Kell <alexander@kell.es>
2020-09-09 17:47:32 +0100

    iterate through sensitivity analysis

--- FTT61x24v8.1FTC/FTTPowerTune.py
+++ FTT61x24v8.1FTC/FTTPowerTune.py
@@ -45,8 +45,8 @@ class AdderServing(gym.Env):
 
 @ray.remote
 def run_ftt_power(port):
-    os.chdir("/Users/alexanderkell/Documents/PhD/Projects/17-ftt-power-reinforcement/FTT61x24v8.1FTC/")
-    # os.chdir("/home/alexander/Documents/reinforcement-learning-investment-ftt-power/FTT61x24v8.1FTC")
+    # os.chdir("/Users/alexanderkell/Documents/PhD/Projects/17-ftt-power-reinforcement/FTT61x24v8.1FTC/")
+    os.chdir("/home/alexander/Documents/reinforcement-learning-investment-ftt-power/FTT61x24v8.1FTC")
     eng = matlab.engine.start_matlab()
 
     time.sleep(10)
