0960c36ee72b6450d8028db2c16c2711aa2b46eb
Alexander Kell <alexander@kell.es>
2021-02-03 20:07:09 +0000

    Update run_FTT_power_from_python.py

--- FTT61x24v8.1FTC/run_FTT_power_from_python.py
+++ FTT61x24v8.1FTC/run_FTT_power_from_python.py
@@ -1,6 +1,7 @@
 import os
 import matlab.engine
 
-os.chdir("/Users/alexanderkell/Documents/PhD/Projects/17-ftt-power-reinforcement/FTT61x24v8.1FTC")
+# os.chdir("/Users/alexanderkell/Documents/PhD/Projects/17-ftt-power-reinforcement/FTT61x24v8.1FTC")
+os.chdir("/home/ps/ce-fs2/akell/PhD/ftt-power/reinforcement-learning-investment-ftt-power/FTT61x24v8.1FTC")
 eng = matlab.engine.start_matlab()
 eng.Run_FTT_Power(port=9912)
