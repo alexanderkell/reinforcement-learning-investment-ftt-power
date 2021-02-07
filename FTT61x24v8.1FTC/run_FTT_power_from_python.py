768b4a635ab523c25ae8cae7471f6332c595841c
Alexander Kell <alexanderkell@alexanders-air.lan>
2021-02-07 11:08:49 +0000

    resolved conflict

--- FTT61x24v8.1FTC/run_FTT_power_from_python.py
+++ FTT61x24v8.1FTC/run_FTT_power_from_python.py
@@ -1,8 +1,8 @@
 import os
 import matlab.engine
 
-# os.chdir("/Users/alexanderkell/Documents/PhD/Projects/17-ftt-power-reinforcement/FTT61x24v8.1FTC")
-os.chdir("/home/ps/ce-fs2/akell/PhD/ftt-power/reinforcement-learning-investment-ftt-power/FTT61x24v8.1FTC")
+os.chdir("/Users/alexanderkell/Documents/PhD/Projects/17-ftt-power-reinforcement/FTT61x24v8.1FTC")
+# os.chdir("/home/ps/ce-fs2/akell/PhD/ftt-power/reinforcement-learning-investment-ftt-power/FTT61x24v8.1FTC")
 eng = matlab.engine.start_matlab()
 # eng.Run_FTT_Power(port=9912)
 eng.Run_FTT_Power(9912, 400, 400)
--- FTT61x24v8.1FTC/run_matlab.m
+++ FTT61x24v8.1FTC/run_matlab.m
@@ -1,2 +1,15 @@
 function result = run_matlab(a, b)
-    result = a+b    
+
+    
+    import ray.rllib.env.policy_client.*
+    import ray.rllib.env.*
+    import gym.*
+    import numpy.*
+    
+    import reinforcement_learning_client_server.*
+
+    address = strcat('http://127.0.0.1:', num2str(9914));
+    client = py.ray.rllib.env.PolicyClient(address);
+
+    eid = client.start_episode();
+
