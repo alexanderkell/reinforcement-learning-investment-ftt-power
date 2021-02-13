2479215eba800423945f1298cbb83fe77a43491b
Alexander Kell <alexanderkell@alexanders-air.lan>
2021-02-13 14:03:45 +0000

    as per eduardo's coments

--- FTT61x24v8.1FTC/run_matlab.m
+++ FTT61x24v8.1FTC/run_matlab.m
@@ -1,15 +1,6 @@
-function result = run_matlab(a, b)
-
-    
-    import ray.rllib.env.policy_client.*
-    import ray.rllib.env.*
-    import gym.*
-    import numpy.*
-    
-    import reinforcement_learning_client_server.*
-
+function result = run_matlab()
+    py.importlib.import_module('ray.rllib.env.policy_client')
     address = strcat('http://127.0.0.1:', num2str(9914));
     client = py.ray.rllib.env.PolicyClient(address);
 
-% eid = client.start_episode();
 
