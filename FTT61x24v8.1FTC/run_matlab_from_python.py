769fe868bff46fdebed9b9662601e562944b196b
Alexander Kell <alexander@kell.es>
2021-03-13 18:59:17 +0000

    trying to get it working

--- FTT61x24v8.1FTC/run_matlab_from_python.py
+++
@@ -1,7 +0,0 @@
-import os
-import matlab.engine
-
-# os.chdir("/Users/alexanderkell/Documents/PhD/Projects/17-ftt-power-reinforcement/FTT61x24v8.1FTC")
-os.chdir("/home/ps/ce-fs2/akell/PhD/ftt-power/reinforcement-learning-investment-ftt-power/FTT61x24v8.1FTC")
-eng = matlab.engine.start_matlab()
-eng.run_matlab(4,5)
